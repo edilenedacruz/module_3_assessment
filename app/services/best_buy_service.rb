4b44556752397519a50ecf10c658dd66a2031f4f
Edilene <edilene_cruz@yahoo.com.br>
2017-04-13 11:12:34 -0600

    missed previous commit

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -2,17 +2,17 @@ class BestBuyService
   attr_reader :connection, :key
   def initialize
     @key = ENV['API_KEY']
-    @connection = Faraday.new("https://api.bestbuy.com/v1/stores")
+    @connection = Faraday.new("https://api.bestbuy.com/v1")
   end
 
   def find_with_zip(zip)
+    a = parse(@connection.get 
     binding.pry
-    parse(connection.get "((area(80202,25)))?apiKey=6tmxnbn286qhnt4act7h3tt8&show=longName,city,fullPostalCode,storeType,phone&format=json" )
   end
 
   private
 
-  def parse
+  def parse(response)
     JSON.parse(response.body, symbolize_names: true)
   end
 end
