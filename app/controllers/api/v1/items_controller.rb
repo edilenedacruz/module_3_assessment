129b2cae18e0494e98e2b38b27ee4702bab7101c
Edilene <edilene_cruz@yahoo.com.br>
2017-04-13 09:32:41 -0600

    implements show for api needs serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: Item.all
   end
+
+  def show
+    render json: Item.find_by(params["id"])
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:id, :name, :description, :image_url)
+  end
 end
--- spec/api/v1/items_spec.rb
+++ spec/api/v1/items_spec.rb
@@ -7,5 +7,23 @@ RSpec.describe "GET" do
 
     expect(response).to be_success
     expect(response.status).to eq(200)
+    # binding.pry
+    # expect(page).to have_content("id")
+    # expect(page).to have_content("name")
+    # expect(page).to have_content("description")
+    # expect(page).to have_content("image_url")
+    # expect(page).to_not have_content("created_at")
+    # expect(page).to_not have_content("updated_at")
   end
+
+  it "gets an individual item" do
+
+    get "/api/v1/items/1"
+
+    expect(response).to be_success
+    expect(response.status).to eq(200)
+
+  end
+
+  it "gets "
 end
